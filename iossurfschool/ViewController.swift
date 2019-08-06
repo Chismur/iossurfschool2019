0b0d8a42dfe4ed0640b3f748291d205dcb3386ec
AnnaKozlova <zzzyyyyy@mail.ru>
2019-08-06 15:19:56 +0300

    Initial Commit

---
+++ iossurfschool/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iossurfschool
+//
+//  Created by xcode on 06.08.2019.
+//  Copyright © 2019 vsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
