c278c769f467f7856c9328fc74fa7dae8d63bf6a
123 <random@gmail.com>
2019-08-08 15:13:21 +0300

    Example table with detailed

---
+++ Screens/FeedTestCell.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  iossurfschool
+//
+//  Created by xcode on 08.08.2019.
+//  Copyright © 2019 vsu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FeedTestCell: UITableViewCell{
+    
+}
--- iossurfschool/ViewController.swift
+++ iossurfschool/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("hello world")
     }
-
-
+    
 }
+    
+    extension ViewController: UITableViewDelegate, UITableViewDataSource
+    {
+        
+        func numberOfSections(in tableView: UITableView) -> Int {
+            return 1
+        }
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return 100
+        }
+        
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "FeedTestCell", for: IndexPath) as? FeedTestCell else {
+                return UITableViewCell()
+                cell.testLabel.text = "IndexPath: section:\(indexPath.section)"
+            }
+        }
+    }
 
